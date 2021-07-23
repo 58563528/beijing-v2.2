73051c9b7ef0177208db68a5daf8e56a116f2fb6
58563528 <jizhuni@126.com>
2021-07-23 22:23:27 +0800

    v2.2.0

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -48,7 +48,7 @@ fi
 echo -e "======================8. 启动JDC========================\n"
 cd /ql/jdc/
 if [[ $ENABLE_WEB_JDC == true ]]; then
-  pm2 start ./jdc/JDC
+  pm2 start JDC
   PIDS=`ps -ef |grep JDC |grep -v grep | awk '{print $2}'`
   if [ "$PIDS" != "" ]; then
     echo "myprocess is runing!"
