914acfbe98d88bcd8888c1d1a82940723d7deebe
58563528 <jizhuni@126.com>
2021-08-20 05:00:21 +0800

    Update config.ts

--- back/api/config.ts
+++ back/api/config.ts
@@ -9,6 +9,7 @@ const route = Router();
 
 export default (app: Router) => {
   app.use('/', route);
+
   route.get(
     '/configs/files',
     async (req: Request, res: Response, next: NextFunction) => {
